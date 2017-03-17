215458ac695721ef1c4be8b6b8e23d8a19775261
buoyantair <buoyantair@gmail.com>
2017-03-17 16:22:12 +0530

    Finished making this theme

--- assets/js/markdown.js
+++ assets/js/markdown.js
@@ -1,9 +1,14 @@
-function runMarkdownCompiler(){
-    var text = document.getElementById("docs").innerText,
-        target = document.getElementById("docs"),
-        converter = new showdown.Converter(),
-        html = converter.makeHtml(text);
+var defaultOptions = showdown.getDefaultOptions();
+defaultOptions.simpleLineBreaks = true;
 
+
+function runMarkdownCompiler(){
+    var text = document.getElementById("docs").innerText;
+        target = document.getElementById("docs"),
+        converter = new showdown.Converter();
+
+    var html = converter.makeHtml(text);
+    console.log(html);
         target.innerHTML = html;
 }
 runMarkdownCompiler();
