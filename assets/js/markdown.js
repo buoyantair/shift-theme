023c99adb3f3f5093b59b5e778c566d111868835
buoyantair <buoyantair@gmail.com>
2017-03-17 13:01:52 +0530

    Previous method was not working, currently I'm using Showdown for compiling markdown in docs

--- assets/js/markdown.js
+++ assets/js/markdown.js
@@ -1,2 +1,9 @@
-var marked = require('marked');
-console.log(marked('I am using __markdown__.'));
+function runMarkdownCompiler(){
+    var text = document.getElementById("docs").innerText,
+        target = document.getElementById("docs"),
+        converter = new showdown.Converter(),
+        html = converter.makeHtml(text);
+
+        target.innerHTML = html;
+}
+runMarkdownCompiler();
